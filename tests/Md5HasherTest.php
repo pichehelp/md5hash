c265af18e376a35ed981e417cb76c1faca3f0527
zhanglong <nice5good@126.com>
2019-02-21 10:24:45 +0800

    add unit suit

--- tests/Md5HasherTest.php
+++ tests/Md5HasherTest.php
@@ -6,7 +6,26 @@
  * Time: 9:26
  */
 
-class Md5HasherTest
+class Md5HasherTest extends \PHPUnit\Framework\TestCase
 {
+    protected $hasher;
 
+    public function setUp()
+    {
+        $this->hasher = new \PichyLaravel\Hasher\Md5Hasher();
+    }
+
+    public function testMd5HasherMake()
+    {
+        $password = md5('long1991');
+        $passwordTwo = $this->hasher->make('long1991');
+        return $this->assertEquals($password, $passwordTwo);
+    }
+
+    public function testMd5HasherCheck()
+    {
+        $password = md5('long1991');
+        $result = $this->hasher->check('long1991', $password);
+        return $this->assertTrue($result);
+    }
 }
